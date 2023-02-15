af84219f6b032bde4acbe58327188f84904666af
angad khera <kheraa6@mcmaster.ca>
2023-02-15 14:58:56 -0500

    __

--- L04/lab4.sh
+++ L04/lab4.sh
@@ -4,10 +4,33 @@
 WDIR="$1"
 cd "$WDIR"
 
+if [ -z "$3" ]; then
+    filename="file1.txt"
+else
+    filename="$3"
+fi
+
 if [ "$2" -eq 1 ] ; then
-    echo "TODO Objective 1"
+    if [ ! -f "$filename" ] ; then
+        echo "$USER" > "$filename"
+    fi
 elif [ "$2" -eq 2 ] ; then
-    echo "TODO Objective 2"
+    if [ -f "$filename" ] ; then
+        if [ ! -d "backup" ] ; then
+            mkdir "backup"
+        fi
+        cp "$filename" "backup/"
+    fi
+elif [ "$2" -eq 3 ] ; then
+    if [ -f "backup/$filename" ] ; then
+        cp "backup/$filename" "$filename"
+        rm "backup/$filename"
+    fi
+fi
+
+if [ "$#" -gt 3 ]; then
+    echo "${@:4}" > excess.txt
 fi
 
 echo "Finished"
+
