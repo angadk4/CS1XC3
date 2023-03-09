cc95d62708fdb0cba39df358383fc2f6fab540fa
angad khera <kheraa6@mcmaster.ca>
2023-03-09 17:41:36 -0500

    p4

---
+++ L06/part4/Lab6Part4.h
@@ -0,0 +1,7 @@
+void barrettsPrivateers(void);
+int sum2(int x, int y);
+int sum3(int x, int y, int z);
+int sum4(int w, int x, int y, int z);
+int prod2(int x, int y);
+int prod3(int x, int y, int z);
+int prod4(int w, int x, int y, int z);
